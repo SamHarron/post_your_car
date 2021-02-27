e4fbe088e77490f0cb6655180d2b5d9c21994ba6
SamHarron <harronsam@gmail.com>
2021-02-27 16:49:55 -0500

    updated the welcome view and stylesheet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-    endable :sessions
+    enable :sessions
     set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
