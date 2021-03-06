a7ee88424fbf234274acf72fb3a89d400d8f6ede
SamHarron <harronsam@gmail.com>
2021-03-06 02:33:21 -0500

    updated users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :cars
-  validates :user_id, presence: true
-  validates :user_id, uniqueness: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
   validates :email, presence: true
   validates :email, uniqueness: true
   has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_064657) do
+ActiveRecord::Schema.define(version: 2021_03_06_072114) do
 
   create_table "cars", force: :cascade do |t|
     t.integer "user_id"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_03_06_064657) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user_id"
+    t.string "username"
     t.string "email"
     t.string "password_digest"
   end
